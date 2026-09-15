ca89fd74aac367aa7d6c4d96d12547837be95e90
Simon Munene <your-github-email@examplemunenesimon254@gmail.com>
2026-09-15 21:45:57 +0300

    Fix workflow YAML, move connection test to Python file

---
+++ data-ingestion/test_connection.py
@@ -0,0 +1,17 @@
+import os
+from neo4j import GraphDatabase
+
+uri  = os.environ["NEO4J_URI"]
+user = os.environ["NEO4J_USER"]
+pwd  = os.environ["NEO4J_PASSWORD"]
+
+print("URI repr :", repr(uri))
+print("USER repr:", repr(user))
+print("PWD chars:", repr(pwd[0]), "...", repr(pwd[-1]), "len:", len(pwd))
+
+driver = GraphDatabase.driver(uri, auth=(user, pwd))
+with driver.session() as s:
+    result = s.run("RETURN 1 AS ok").single()["ok"]
+    print("Query result:", result)
+driver.close()
+print("CONNECTION OK")
